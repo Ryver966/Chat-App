a5e2d032936b153ff64a6d85bb02990a81fb3da0
Ryver966 <p.gorski-96@wp.pl>
2017-02-05 15:26:30 +0100

    Chat App

---
+++ JS/login_screen.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyDMu9HckPqtfindUTsuldrjIa4wce2jZ2g",
+    authDomain: "chat-app-2fe3b.firebaseapp.com",
+    databaseURL: "https://chat-app-2fe3b.firebaseio.com",
+    storageBucket: "chat-app-2fe3b.appspot.com",
+    messagingSenderId: "694196290722"
+};
+firebase.initializeApp(config);

a09e24d4129d173f19891b1f2757ac34c3f3e41c
Ryver966 <p.gorski-96@wp.pl>
2017-02-06 13:25:32 +0100

    Chat App

--- JS/login_screen.js
+++ JS/login_screen.js
@@ -1,9 +1,18 @@
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyDMu9HckPqtfindUTsuldrjIa4wce2jZ2g",
-    authDomain: "chat-app-2fe3b.firebaseapp.com",
-    databaseURL: "https://chat-app-2fe3b.firebaseio.com",
-    storageBucket: "chat-app-2fe3b.appspot.com",
-    messagingSenderId: "694196290722"
-};
-firebase.initializeApp(config);
+window.onload = function () {
+    const config = {
+        apiKey: "AIzaSyDMu9HckPqtfindUTsuldrjIa4wce2jZ2g",
+        authDomain: "chat-app-2fe3b.firebaseapp.com",
+        databaseURL: "https://chat-app-2fe3b.firebaseio.com",
+        storageBucket: "chat-app-2fe3b.appspot.com",
+        messagingSenderId: "694196290722"
+    };
+    firebase.initializeApp(config);
+}
+
+function pressEnter(event, element){
+    const x = event.keyCode();
+    if(x === 13){
+        const button = document.getElementsByClassName(element)[0];
+        button.clicked();
+    }
+}

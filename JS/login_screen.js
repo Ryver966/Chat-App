70b7ec41424dfcae14ec384c543b0747c767726e
Ryver966 <p.gorski-96@wp.pl>
2017-02-06 13:56:23 +0100

    Chat App

--- JS/login_screen.js
+++ JS/login_screen.js
@@ -1,18 +1,12 @@
-window.onload = function () {
-    const config = {
-        apiKey: "AIzaSyDMu9HckPqtfindUTsuldrjIa4wce2jZ2g",
-        authDomain: "chat-app-2fe3b.firebaseapp.com",
-        databaseURL: "https://chat-app-2fe3b.firebaseio.com",
-        storageBucket: "chat-app-2fe3b.appspot.com",
-        messagingSenderId: "694196290722"
-    };
-    firebase.initializeApp(config);
-}
-
 function pressEnter(event, element){
-    const x = event.keyCode();
+    const x = event.keyCode;
     if(x === 13){
         const button = document.getElementsByClassName(element)[0];
-        button.clicked();
-    }
+        button.click();
+    };
+};
+
+function signIn (){
+    const firebaseRef = firebase.database().ref();
+    firebaseRef.child('User').set('value');
 }
